e85928d4d4e58becb4c440dc55fc31e29e1fed6b
root <root@vultr.guest>
2024-03-06 08:26:06 +0000

    add save images

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 docker buildx build -t template_run:v0.1 -f template_runtime/Dockerfile .
 docker buildx build -t alpine_runtime:v0.2 -f markdown_format_runtime/Dockerfile .
+mkdir /tmp/docker_images
+docker save -o /tmp/docker_images/template_run_v0.1.tar template_run:v0.1
+docker save -o /tmp/docker_images/alpine_runtime_v0.2.tar alpine_runtime:v0.2
